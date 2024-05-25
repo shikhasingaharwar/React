b0490187929b968b0bf04e7f556d99918f70b1b1
shikha <shikhasgahrwar149@gmail.com>
2024-05-25 23:13:12 +0530

    Initial commit

---
+++ Project1/Colorchanger.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector("body");
+
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+console.log(e);
+console.log(e.target);
+if(e.target.id === "grey"){
+    body.style.backgroundColor="grey";
+}
+if(e.target.id === "green"){
+    body.style.backgroundColor="green";
+}
+if(e.target.id === "blue"){
+    body.style.backgroundColor="blue";
+}
+if(e.target.id === "red"){
+    body.style.backgroundColor="red";
+}
+   
+    })
+
+});
